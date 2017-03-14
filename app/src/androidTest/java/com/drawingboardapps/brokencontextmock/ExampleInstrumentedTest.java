5b9e0074cc88032dff0fac438b6faed235e5386a
Zachary Nwabudike <zach@rokmobile.com>
2017-03-14 11:34:08 -0700

    Can't do that

--- app/src/androidTest/java/com/drawingboardapps/brokencontextmock/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/drawingboardapps/brokencontextmock/ExampleInstrumentedTest.java
@@ -6,6 +6,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyInt;
@@ -16,14 +17,19 @@ import static org.mockito.Mockito.doReturn;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+//Can't do this - MockitoJUnitRunner is only for emulated tests
+//@RunWith(MockitoJUnitRunner.class)
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        doReturn("").when(appContext).getString(anyInt());
-        
+
+        //Can't do this 
+//        doReturn("").when(appContext).getString(anyInt());
+
         assertEquals("com.drawingboardapps.brokencontextmock", appContext.getPackageName());
         assertEquals("", appContext.getString(0));
     }
